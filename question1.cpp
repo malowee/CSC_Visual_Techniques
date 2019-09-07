cfee8c5934e2ad5b779e3189220ceaccb4dc9906
malowee <32024370+malowee@users.noreply.github.com>
2019-09-06 22:59:06 -0700

    Create question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+//function to iterate
+int iterate()
+  {
+  for(int i = 0; i < 10; i++)
+    {
+    for(int j = 0; j < 10; j++)
+      {
+      float sum;
+      float a = i * 100;
+      float b = j * 0.01;
+      
+      //-67.9- quotient is 72 i.e a67.9b
+      //brute force through the values of a and b
+      //then divide each value by 72 and check for remainders
+      
+      sum = a + 69.7 +b;
+      
+      double check;
+      check = sum % 72;
+      
+      cout<<sum/n<<check/n;
+      }
+    }
+  }
+  
+  int main()
+  {
+  iterate();
+  return;
+  }
